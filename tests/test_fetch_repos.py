ce8a68f5407b27809c8ce17b9071067f18786886
Hedy Li <hedyhyry@gmail.com>
2020-08-02 16:28:21 +0800

    better tests

--- tests/test_fetch_repos.py
+++ tests/test_fetch_repos.py
@@ -3,4 +3,11 @@ from gtrending import fetch_repos
 
 def test_fetch_repos():
     res = fetch_repos()
-    print(res)
+    for repo in res:
+        assert isinstance(repo["name"], str)
+        assert isinstance(repo["author"], str)
+        assert repo["stars"] >= 0
+        assert repo["forks"] >= 0
+        assert repo["url"] == f"https://github.com/{repo['author']}/{repo['name']}"
+        assert isinstance(repo["description"], str)
+        assert repo["currentPeriodStars"] >= 0
