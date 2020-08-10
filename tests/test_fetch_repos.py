513d889e6a37adcac02b39100b5aec6ea348c7a6
Hedy Li <hedyhyry@gmail.com>
2020-08-10 08:14:45 +0000

    tests: improve repo tests

--- tests/test_fetch_repos.py
+++ tests/test_fetch_repos.py
@@ -1,9 +1,8 @@
 from gtrending import fetch_repos
 
 
-def test_fetch_repos():
-    res = fetch_repos()
-    for repo in res:
+def basic_assertions(repos, language=""):
+    for repo in repos:
         assert isinstance(repo["name"], str)
         assert isinstance(repo["author"], str)
         assert repo["stars"] >= 0
@@ -11,3 +10,23 @@ def test_fetch_repos():
         assert repo["url"] == f"https://github.com/{repo['author']}/{repo['name']}"
         assert isinstance(repo["description"], str)
         assert repo["currentPeriodStars"] >= 0
+        if "language" in str(repo.keys()):
+            print(repo["language"])
+            assert isinstance(repo["language"], str)
+        if language:
+            assert repo["language"].lower() == language
+        if "languageColor" in str(repo.keys()):
+            assert len(repo["languageColor"]) == 7
+            assert repo["languageColor"].startswith("#")
+
+
+def test_all():
+    res = fetch_repos()
+    basic_assertions(res)
+
+
+def test_language():
+    res = fetch_repos(language="python")
+    basic_assertions(res, "python")
+    res = fetch_repos(language="javascript")
+    basic_assertions(res, "javascript")
