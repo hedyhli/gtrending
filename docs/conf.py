b6d8d2f86f180f8753c097aee61c1b75bb544a87
Hedy Li <hedyhyry@gmail.com>
2020-08-11 10:29:29 +0000

    update requirements

--- docs/conf.py
+++ docs/conf.py
@@ -46,10 +46,11 @@ master_doc = 'index'
 import guzzle_sphinx_theme
 
 html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
 
 # Register the theme as an extension to generate a sitemap.xml
 extensions.append("guzzle_sphinx_theme")
+
+html_theme = 'guzzle_sphinx_theme'
 
 # Guzzle theme options (see theme.conf for more information)
 html_theme_options = {
