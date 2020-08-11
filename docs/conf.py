657ef104eee754cabf5c13dc7e4a625292bd4c8b
Hedy Li <hedyhyry@gmail.com>
2020-08-11 10:06:56 +0000

    guzzle theme

--- docs/conf.py
+++ docs/conf.py
@@ -27,11 +27,25 @@ release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 
+extensions = []
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+
+import guzzle_sphinx_theme
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "gtrending",
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
