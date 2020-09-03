d45918cc81c944ce90927ee589eb429f3365286b
ByMykel <bymykel5@gmail.com>
2020-09-03 17:41:26 +0200

    Update __init__.py

--- gtrending/__init__.py
+++ gtrending/__init__.py
@@ -1,1 +1,1 @@
-from .fetch import fetch_repos, fetch_developers, languages_list, spoken_languages_list
+from .fetch import fetch_repos, fetch_developers, languages_list, spoken_languages_list, check_language, check_spoken_language, check_since
