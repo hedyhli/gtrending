23a8eed11f560e307f7a010a4fb6f81a38e851b9
Hedy Li <hedyhyry@gmail.com>
2021-02-18 06:35:37 +0000

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -46,6 +46,7 @@ import sphinx_rtd_theme
 
 # Register the theme as an extension to generate a sitemap.xml
 extensions = [
+    'sphinx.ext.autodoc',
     "sphinx_rtd_theme",
 ]
 
