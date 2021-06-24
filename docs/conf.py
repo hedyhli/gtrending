7d7824de8b6ae906769e351799b8ca3feee8d1f0
Hedy Li <hedyhyry@gmail.com>
2021-06-24 12:59:52 +0800

    bump version in conf.py [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Hedy Li"
 author = "Hedy Li"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.3.2"
 
 
 # -- General configuration ---------------------------------------------------
