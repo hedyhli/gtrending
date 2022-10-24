3244150e44413fb8de7c91152759b12bb134098d
Ankit S <ankit.s@postman.com>
2022-10-24 12:24:51 +0530

    Adding extra tests

---
+++ tests/cli/test_errors.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_no_command(run_cli):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        run_cli('')
+    assert pytest_wrapped_e.value.code != 0
+
+def test_unknown_command(run_cli):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        run_cli('foo')
+    assert pytest_wrapped_e.value.code != 0
+
+def test_unknown_arg(run_cli):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        run_cli('repos --home')
+    assert pytest_wrapped_e.value.code != 0
+
+def test_unknown_arg_value(run_cli):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        run_cli('repos --since yearly')
+    assert pytest_wrapped_e.value.code != 0
